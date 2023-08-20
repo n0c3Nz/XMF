1e62a2ebfc8321d4590aa3d5bae5ba52aff6bf30
c3Nz <106092515+n0c3Nz@users.noreply.github.com>
2023-08-20 20:33:08 +0200

    Create xml-filter.py

---
+++ xml-filter.py
@@ -0,0 +1,33 @@
+import sys
+import xml.etree.ElementTree as ET      
+                              
+def main():
+	if len(sys.argv) < 3:
+		print("""
+██╗  ██╗███╗   ███╗███████╗
+╚██╗██╔╝████╗ ████║██╔════╝
+ ╚███╔╝ ██╔████╔██║█████╗  
+ ██╔██╗ ██║╚██╔╝██║██╔══╝  
+██╔╝ ██╗██║ ╚═╝ ██║██║     Version 1.0 - Don't mess with c3Nz!
+╚═╝  ╚═╝╚═╝     ╚═╝╚═╝     Made by c3Nz (@n0c3Nz)
+                           """)
+		print("Uso: python script.py archivo.xml nombre_atributo1 nombre_atributo2 ...")
+		return
+	filename = sys.argv[1]
+	attribute_names = sys.argv[2:]
+
+	tree = ET.parse(filename)
+	root = tree.getroot()
+
+	elements_lists = [root.findall('.//*[@{}]'.format(attr_name)) for attr_name in attribute_names]
+	
+	if all(elements for elements in elements_lists):
+		num_elements = len(elements_lists[0])
+		for i in range(num_elements):
+			values = [elements[i].get(attribute_names[j]) for j, elements in enumerate(elements_lists)]
+			print(" ".join(values))
+	else:
+		print("No se encontraron elementos para todos los atributos proporcionados.")
+
+if __name__ == "__main__":
+	main()
